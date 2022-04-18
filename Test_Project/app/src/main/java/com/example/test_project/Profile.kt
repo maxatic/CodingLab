9cbb4f5128231f2449ed539be0dd85db0470c4fd
johanessson <foreverwantlive@gmail.com>
2022-04-18 19:10:40 +0600

    created login layout

---
+++ Test_Project/app/src/main/java/com/example/test_project/Profile.kt
@@ -0,0 +1,13 @@
+package com.example.test_project
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Profile : AppCompatActivity () {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
